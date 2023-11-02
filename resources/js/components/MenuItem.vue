ab466f06da1cab046d0a1e36ff3785e857202f38
Ondrej Ehrlich <ehrlich@snadnee.com>
2023-11-02 13:53:30 +0100

    Bugfix: Displaying menu items

--- resources/js/components/MenuItem.vue
+++ resources/js/components/MenuItem.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="sidebar-item">
+  <div>
     <component
-      :is="component"
-      v-bind="linkAttributes"
-      class="sidebar-item-title relative"
-      :class="{ 'inertia-link-active': item.active }"
-      @click="handleClick"
+        :is="component"
+        v-bind="linkAttributes"
+        class="w-full flex min-h-8 px-1 py-1 rounded text-left text-gray-500 dark:text-gray-500 focus:outline-none focus:ring focus:ring-primary-200 dark:focus:ring-gray-600 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-800"
+        :data-active-link="item.active"
+        :class="{
+        'font-bold text-primary-500 dark:text-primary-500': item.active,
+      }"
+        @click="handleClick"
     >
 
-      <span class="sidebar-item-icon" />
-      <span class="sidebar-item-label flex items-center">
-
+      <span class="inline-block shrink-0 w-6 h-6" />
+      <span class="flex-1 flex items-center w-full px-3 text-sm">
         {{ item.name }}
+      </span>
 
-        <span v-if="item.badge" class="absolute right-0 ml-2 mr-4">
-          <Badge :extra-classes="item.badge.typeClass" class="">
-            {{ item.badge.value }}
-          </Badge>
-        </span>
+      <span class="inline-block h-6 shrink-0">
+        <Badge v-if="item.badge" :extra-classes="item.badge.typeClass">
+          {{ item.badge.value }}
+        </Badge>
+      </span>
 
 
 
-        <Tooltip v-if="tooltip!==null" class="inline absolute right-0">
+      <Tooltip v-if="tooltip!==null" class="inline absolute right-0">
 
         <template v-slot:content >
           <span v-html="tooltip"></span>
@@ -34,8 +37,6 @@
         />
 
       </Tooltip>
-
-      </span>
     </component>
   </div>
 </template>
@@ -45,7 +46,7 @@ import identity from 'lodash/identity'
 import isNull from 'lodash/isNull'
 import omitBy from 'lodash/omitBy'
 import pickBy from 'lodash/pickBy'
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters, mapMutations } from '../../../node_modules/vuex'
 
 export default {
   props: {
@@ -94,18 +95,18 @@ export default {
       let method = this.requestMethod
 
       return pickBy(
-        omitBy(
-          {
-            href: this.item.path,
-            method: method !== 'GET' ? method : null,
-            headers: this.item.headers || null,
-            data: this.item.data || null,
-            rel: this.component === 'a' ? 'noreferrer noopener' : null,
-            target: this.item.target || null,
-          },
-          isNull
-        ),
-        identity
+          omitBy(
+              {
+                href: this.item.path,
+                method: method !== 'GET' ? method : null,
+                headers: this.item.headers || null,
+                data: this.item.data || null,
+                rel: this.component === 'a' ? 'noreferrer noopener' : null,
+                target: this.item.target || null,
+              },
+              isNull
+          ),
+          identity
       )
     },
   },
