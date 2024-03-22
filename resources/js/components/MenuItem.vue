97e916d353f8815b86befbcf321ba57ba645324d
ca-morfeusz2844 <107105981+ca-morfeusz2844@users.noreply.github.com>
2024-03-22 12:13:41 +0100

    Fix/menu item (#1)

--- resources/js/components/MenuItem.vue
+++ resources/js/components/MenuItem.vue
@@ -1,43 +1,44 @@
 <template>
-  <div class="sidebar-item">
-    <component
-      :is="component"
-      v-bind="linkAttributes"
-      class="sidebar-item-title relative"
-      :class="{ 'inertia-link-active': item.active }"
-      @click="handleClick"
-    >
+    <div>
+        <component
+            :is="component"
+            v-bind="linkAttributes"
+            class="w-full flex min-h-8 px-1 py-1 rounded text-left text-gray-500 dark:text-gray-500 focus:outline-none focus:ring focus:ring-primary-200 dark:focus:ring-gray-600 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-800"
+            :data-active-link="item.active"
+            :class="{
+        'font-bold text-primary-500 dark:text-primary-500': item.active,
+      }"
+            @click="handleClick"
+        >
 
-      <span class="sidebar-item-icon" />
-      <span class="sidebar-item-label flex items-center">
-
+            <span class="inline-block shrink-0 w-6 h-6"/>
+            <span class="flex-1 flex items-center w-full px-3 text-sm">
         {{ item.name }}
-
-        <span v-if="item.badge" class="absolute right-0 ml-2 mr-4">
-          <Badge :extra-classes="item.badge.typeClass" class="">
-            {{ item.badge.value }}
-          </Badge>
-        </span>
-
-
-
-        <Tooltip v-if="tooltip!==null" class="inline absolute right-0">
-
-        <template v-slot:content >
-          <span v-html="tooltip"></span>
-        </template>
-
-        <Icon
-            :solid="true"
-            type="question-mark-circle"
-            class="cursor-pointer text-gray-400 dark:text-gray-500"
-        />
-
-      </Tooltip>
-
       </span>
-    </component>
-  </div>
+
+            <span class="inline-block h-6 shrink-0">
+        <Badge v-if="item.badge" :extra-classes="item.badge.typeClass">
+          {{ item.badge.value }}
+        </Badge>
+      </span>
+
+
+            <Tooltip v-if="tooltip!==null" class="inline absolute right-0">
+
+                <template v-slot:content>
+                    <span v-html="tooltip"></span>
+                </template>
+
+                <Icon
+                    :solid="true"
+                    type="question-mark-circle"
+                    class="cursor-pointer text-gray-400 dark:text-gray-500"
+                />
+
+            </Tooltip>
+
+        </component>
+    </div>
 </template>
 
 <script>
@@ -45,69 +46,69 @@ import identity from 'lodash/identity'
 import isNull from 'lodash/isNull'
 import omitBy from 'lodash/omitBy'
 import pickBy from 'lodash/pickBy'
-import { mapGetters, mapMutations } from 'vuex'
+import {mapGetters, mapMutations} from 'vuex'
 
 export default {
-  props: {
-    item: {
-      type: Object,
-      required: true,
-    },
-  },
-
-  methods: {
-    ...mapMutations(['toggleMainMenu']),
-
-    handleClick() {
-      if (this.mainMenuShown) {
-        this.toggleMainMenu()
-      }
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
     },
 
+    methods: {
+        ...mapMutations(['toggleMainMenu']),
 
-  },
+        handleClick() {
+            if (this.mainMenuShown) {
+                this.toggleMainMenu()
+            }
+        },
 
-  computed: {
-    ...mapGetters(['mainMenuShown']),
 
-    requestMethod() {
-      return this.item.method || 'GET'
     },
 
-    component() {
-      if (this.requestMethod !== 'GET') {
-        return 'FormButton'
-      } else if (this.item.external !== true) {
-        return 'Link'
-      }
+    computed: {
+        ...mapGetters(['mainMenuShown']),
 
-      return 'a'
+        requestMethod() {
+            return this.item.method || 'GET'
+        },
+
+        component() {
+            if (this.requestMethod !== 'GET') {
+                return 'FormButton'
+            } else if (this.item.external !== true) {
+                return 'Link'
+            }
+
+            return 'a'
+        },
+
+        tooltip() {
+            if (this.item.data)
+                return this.item.data.tooltip;
+            return null;
+        },
+
+        linkAttributes() {
+            let method = this.requestMethod
+
+            return pickBy(
+                omitBy(
+                    {
+                        href: this.item.path,
+                        method: method !== 'GET' ? method : null,
+                        headers: this.item.headers || null,
+                        data: this.item.data || null,
+                        rel: this.component === 'a' ? 'noreferrer noopener' : null,
+                        target: this.item.target || null,
+                    },
+                    isNull
+                ),
+                identity
+            )
+        },
     },
-
-    tooltip() {
-      if (this.item.data)
-        return this.item.data.tooltip;
-      return null;
-    },
-
-    linkAttributes() {
-      let method = this.requestMethod
-
-      return pickBy(
-        omitBy(
-          {
-            href: this.item.path,
-            method: method !== 'GET' ? method : null,
-            headers: this.item.headers || null,
-            data: this.item.data || null,
-            rel: this.component === 'a' ? 'noreferrer noopener' : null,
-            target: this.item.target || null,
-          },
-          isNull
-        ),
-        identity
-      )
-    },
-  },
 }
 </script>
